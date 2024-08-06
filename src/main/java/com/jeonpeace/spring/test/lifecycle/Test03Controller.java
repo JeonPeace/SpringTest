4885a75ae5c19904562416ec7441a53f284c1698
megait402 <megait402@gmail.com>
2024-08-06 21:47:28 +0900

    [전진우] - thymeleaf 설정 및 test03 추가

---
+++ src/main/java/com/jeonpeace/spring/test/lifecycle/Test03Controller.java
@@ -0,0 +1,16 @@
+package com.jeonpeace.spring.test.lifecycle;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class Test03Controller {
+
+	@RequestMapping("/lifecycle/test03/1")
+	public String thymeleafTest03() {
+		
+		return "lifecycle/test03";
+		
+	}
+	
+}
